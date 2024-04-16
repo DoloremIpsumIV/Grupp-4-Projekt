c90e4c66bc16a9691cc25691640524acda7d6b68
Rasmus Jildholt <Rjildholt@gmail.com>
2024-04-16 16:53:23 +0200

    Location defaults to Linne if user doesn't share

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-const vaxjo = {
-    name: "Växjö",
-    lat: 56.8795,
-    lng: 14.8055,
+const linne = {
+    name: "LinnéUniversitetet",
+    lat: 56.85462,
+    lng: 14.83038,
     zoom: 16
 }                           // Start position for the map
 let map;                    // Variable for the map
 const ApiKey = "vxJzsf1d";  // Api key for SMAPI
-let latitude;               // Latitude of user
-let longitude;              // Longitude of user
+let latitude = linne.lat;   // Latitude of user
+let longitude = linne.lng;  // Longitude of user
 
 // Init function
 function init() {
@@ -20,7 +20,7 @@ window.addEventListener("load", init);
 
 // Function for initiation of the map
 function initMap(id) {
-    map = L.map(id).setView([vaxjo.lat, vaxjo.lng], vaxjo.zoom);
+    map = L.map(id).setView([linne.lat, linne.lng], linne.zoom);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -43,11 +43,11 @@ function updateMapLoc(latitude, longitude) {
 }
 
 // Async function that collects restaurant data
-async function fetchData(){
-    let response = await fetch("https://smapi.lnu.se/api/?api_key=vxJzsf1d&debug=true&controller=food&method=getFromLatLng&lat=" + latitude + "&lng=" + longitude + "&radius=1")
-    if (response.ok){
+async function fetchData() {
+    let response = await fetch("https://smapi.lnu.se/api/?api_key=" + ApiKey + "&controller=food&method=getFromLatLng&lat=" + latitude + "&lng=" + longitude + "&radius=1")
+    if (response.ok) {
         let dataResponse = await response.json();
-        console.log (dataResponse.payload);
+        console.log(dataResponse.payload);
     }
     else console.log = "Error during fetch: " + response.status;
 }
