ad4125e3d1ded066c9c42ff5c87d404b54fc226c
Rasmus Jildholt <Rjildholt@gmail.com>
2024-04-28 21:05:33 +0200

    Merge from main

--- js/script.js
+++ js/script.js
@@ -67,28 +67,6 @@ async function fetchData() {
     else console.log("Error during fetch: " + response.status);
 }
 
-// Function that displays data using a list 
-function showData(json) {
-    const jsonArray = json.payload;
-
-    let htmlCode = " ";
-    for (let i = 0; i < jsonArray.length; i++) {
-        let restaurant = jsonArray[i];
-
-        const rating = parseFloat(restaurant.rating);
-
-        htmlCode +=
-            "<div>" +
-            "<h4>" + restaurant.name + "</h4>" +
-            "<p>" + restaurant.description + "</p>" +
-            "<p>" + rating.toFixed(1) + " Stjärnor</p>" +
-            "<p>" + restaurant.avg_lunch_pricing + " kr</p>" + "</div>";
-
-    }
-
-    document.querySelector("#restaurantInfo").innerHTML = htmlCode;
-}
-
 // Function for changing sticky header
 function stickyHeader() {
     if (window.scrollY > position && !flag) {
@@ -98,5 +76,62 @@ function stickyHeader() {
     else if (window.scrollY == position) {
         header.classList.remove("stickyHeader");
         flag = false;
+    }
+}
+
+// Function that displays data using a list 
+function showData(json) {
+    const restaurantContainer = document.querySelector("#restaurantInfo");
+    const jsonArray = json.payload;
+    const divElement = document.createElement("div");
+    divElement.id = "restaurantInfo";
+    const elementCreator = new CreateElements(jsonArray);                  // Object that is used to construct elements on website
+
+    for (let i = 0; i < jsonArray.length; i++) {                           // Loop that constructs elements on page
+        const listElements = document.createElement("div");
+        listElements.appendChild(elementCreator.createTitleElement(i));
+        listElements.appendChild(elementCreator.createParagraphElement(i));
+        divElement.appendChild(listElements);
+    }
+    restaurantContainer.appendChild(divElement);
+}
+
+// Class that constructs any element based on method used
+class CreateElements {
+
+    constructor(data) {
+        this.data = data;                                                   // Uses the array that contains all json data
+    }
+
+    createParagraphElement(index) {
+        const fragment = new DocumentFragment();
+        const propertyToShow = ['description', 'type', 'rating', 'sub_type', 'distance_in_km'];            // Data that will be displayed
+        const data = Object.keys(this.data[index]);
+
+        for (let i = 0; i < propertyToShow.length; i++) {
+            const property = propertyToShow[i];
+            const paragraphElement = document.createElement("p");
+            paragraphElement.innerText = property + ": " + this.data[index][property];
+            fragment.appendChild(paragraphElement);
+        }
+
+        for (let i = 0; i < data.length; i++) {                           // Loop that will display all data   
+            const key = data[i];                                          // Takes the array of json data and produces all tags, example: id, name, rating, type, etc.
+            console.log(data[i] + ": " + (this.data[index][key]))
+
+            //    const paragraphElement = document.createElement("p");
+            //    paragraphElement.innerText = data[i] + ": " + (this.data[index][key]);
+            //    fragment.appendChild(paragraphElement);
+            //
+            //    paragraphElement.innerText = this.data[index].description;  // Changing the output of data can be done by changing what comes after the this.data[index] statement
+        }
+        return fragment;
+    }
+
+    createTitleElement(index) {
+        const titleElement = document.createElement("h4");
+        titleElement.innerText = this.data[index].name;                     // Changing the output of data can be done by changing what comes after the this.data[index] statement
+
+        return titleElement;
     }
 }
