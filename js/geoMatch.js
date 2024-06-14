6d21659f07390311fa5f6ea4b930274262eaa6f9
FilippaKD <fd222hz@student.lnu.se>
2024-06-14 19:04:45 +0200

    Geomatch påbörjad

---
+++ js/geoMatch.js
@@ -0,0 +1,12 @@
+let imageFolder = "mapIconsSVG"; //Mappen med svg bilderna
+
+let imagesUsed = [
+    "A_LA_CARTE.svg",
+    "asian.svg",
+    "burgers.svg",
+    "HOT_DOGS.svg",
+    "latin.svg",
+    "MEDITERRANEAN.svg",
+    "PASTRIES.svg",
+    "pizza.svg"
+]; //Bilderna som ska användas
