7509a64bc1721be8f1f79ae9e74a46e600497987
Oscar Ekberg <oscarekberg@hotmail.com>
2024-06-17 13:44:05 +0200

    added trashcans and fixed storage of both lists

--- js/loadSavedRes.js
+++ js/loadSavedRes.js
@@ -1,31 +1,87 @@
 // Funktion som laddar in/upp datan som användaren sparat lokalt i savedBox elementet
 // Behöver skriva en if-sats som kollar ifall den redan finns i localstorage
 
-console.log("loadsavedres")
-document.addEventListener("DOMContentLoaded", () => {
-    const savedBox = document.querySelector("#savedBox");
-    let savedRestaurant = JSON.parse(localStorage.getItem("savedRestaurant")) || [];
-
-    for (let i = 0; i < savedRestaurant.length; i++) {
-        const savedListElements = document.createElement("div");
-        savedListElements.innerHTML = savedRestaurant[i];
-        savedBox.appendChild(savedListElements);
-    }
-});
 
 document.addEventListener("DOMContentLoaded", () => {
+    loadSavedList();
+
     let dragElems = document.querySelectorAll("#savedBox div.restaurantCard");
     console.log(dragElems);
     for (let i = 0; i < dragElems.length; i++) {
         dragElems[i].draggable = true;
         dragElems[i].addEventListener("dragstart", dragStart);
     }
+
+    loadCustomList();
+    removeRestaurant();
 });
+
+function loadSavedList() {
+    const savedBox = document.querySelector("#savedBox");
+    const savedRestaurant = JSON.parse(localStorage.getItem("savedRestaurant")) || [];
+    savedBox.innerHTML = " ";
+
+    for (let i = 0; i < savedRestaurant.length; i++) {
+        const savedListElements = document.createElement("div");
+        savedListElements.innerHTML = savedRestaurant[i];
+        savedBox.appendChild(savedListElements);
+    }
+
+    let trashCans = document.querySelectorAll("#saveBtnIndex");
+    
+    for (let i = 0; i < trashCans.length; i++) {
+        trashCans[i].src = "/images/soptunna.svg";
+    }
+}
+
+function loadCustomList() {
+    const savedListArray = JSON.parse(localStorage.getItem("savedListArray")) || [];
+    const dropElem = document.querySelector("#listBox");
+    dropElem.innerHTML = " ";
+
+    for (let i = 0; i < savedListArray.length; i++) {
+        const div = document.createElement("div");
+        card = savedListArray[i];
+        div.innerHTML = card;
+        dropElem.appendChild(div.firstChild);
+    }
+}
+
+function removeRestaurant() {
+    let trashCansFavorites = document.querySelectorAll("#savedBox #saveBtnIndex");
+    console.log(trashCansFavorites)
+
+    for (let i = 0; i < trashCansFavorites.length; i++) {
+        trashCansFavorites[i].addEventListener("click", () => {
+            removeFromFavoritesList(i)
+            console.log("klick")
+            loadAll();
+        });
+    }
+
+    let trashCansCustom = document.querySelectorAll("#listBox #saveBtnIndex");
+    console.log(trashCansCustom);
+
+    for (let i = 0; i < trashCansCustom.length; i++) {
+        trashCansCustom[i].addEventListener("click", () => {
+            removeFromCustomList(i);
+            console.log("klick i custom")
+            loadAll();
+        })
+    }
+}
+
+function loadAll() {
+    loadSavedList();
+    loadCustomList();
+    removeRestaurant();
+   
+}
 
 function dragStart() {
     let dragElem = this;
     dragElem.draggable = true;
-
+    
     const dropElem = document.querySelector("#listBox");
     console.log(dropElem);
 
@@ -59,6 +115,7 @@ function dragStart() {
                 dragElem.classList.remove(".restaurantCard")
                 break;
             case "drop":
+            
                 dropElem.classList.remove("highlight");
 
                 const clonedListElement = dragElem.cloneNode(true);
@@ -69,7 +126,7 @@ function dragStart() {
                 dragElem.parentNode.removeChild(dragElem);
 
                 updateLocalStorage();
-                removeFromLocalStorage(dragElem.innerHTML);
+                removeRestaurant();
                 break;
         }
     }
@@ -80,36 +137,31 @@ function updateLocalStorage() {
     const restaurantCards = dropElem.querySelectorAll(".restaurantCard");
     console.log(restaurantCards);
     const savedListArray = [];
-    console.log(savedListArray);
+
     for (let i = 0; i < restaurantCards.length; i++) {
         card = restaurantCards[i];
         savedListArray.push(card.outerHTML);
     }
 
     localStorage.setItem("savedListArray", JSON.stringify(savedListArray));
+
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const savedListArray = JSON.parse(localStorage.getItem("savedListArray")) || [];
-    const dropElem = document.querySelector("#listBox");
-
-    for (let i = 0; i < savedListArray.length; i++) {
-        const div = document.createElement("div");
-        card = savedListArray[i];
-
-        div.innerHTML = card;
-        dropElem.appendChild(div.firstChild);
-    }
-
-})
-
-function removeFromLocalStorage(html) {
+function removeFromFavoritesList(index) {
     const savedRestaurant = JSON.parse(localStorage.getItem("savedRestaurant")) || [];
-    const index = savedRestaurant.findIndex(item => item === html);
 
+    console.log(index)
     savedRestaurant.splice(index, 1);
 
     localStorage.setItem("savedRestaurant", JSON.stringify(savedRestaurant));
 
-    // förstår ej varför den alltid endast tar bort det sista indexet i arrayen
+}
+
+function removeFromCustomList(index) {
+    const savedListArray = JSON.parse(localStorage.getItem("savedListArray")) || [];
+
+    console.log(index)
+    savedListArray.splice(index, 1);
+
+    localStorage.setItem("savedListArray", JSON.stringify(savedListArray));
 }
