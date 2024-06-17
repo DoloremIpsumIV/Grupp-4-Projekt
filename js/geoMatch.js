c0f7c31ad97d3111e24440147cb52359e8addd44
FilippaKD <fd222hz@student.lnu.se>
2024-06-17 16:20:23 +0200

    geoMatch karta och början

--- js/geoMatch.js
+++ js/geoMatch.js
@@ -21,16 +21,42 @@ let map;
 
 let miniMap;
 
+let latitude, longitude;
+
+const smaland = {                 // Start position for the map
+    name: "Småland",
+    lat: 56.87767,
+    lng: 14.80906,
+    zoom: 12
+}
+const oland = {                   // Öland coordinates
+    name: "Öland",
+    lat: 56.6499,
+    lng: 16.46859,
+    zoom: 10
+}
+const boundries = {               // Const with min and max boundries for the map
+    maxLatCorner: 56.018610,
+    maxLngCorner: 17.472606,
+    minLatCorner: 58.122646,
+    minLngCorner: 13.061037
+}
+
 
 function init () {
 
     playBtn = document.querySelector("#playButton");
-
-    playBtn.addEventListener("click", startGame);
+    playBtn.addEventListener("click", gameSettings);
 
     let mapBtn = document.querySelector("#mapBtn2");
+    mapBtn.addEventListener("click", openMapDialog);
 
-    mapBtn.addEventListener("click", openMap);
+    let findBtn = document.querySelector("#findBtn2");
+    findBtn.addEventListener("click", startGame);
+
+    let mapPlayBtn =document.querySelector("#mapPlaybtn");
+    mapPlayBtn.addEventListener("click", startGame);
+
 
 
 }
@@ -38,104 +64,105 @@ window.addEventListener("load", init);
 
 
 
-function startGame() {
+function gameSettings() {
     console.log("FGH")
 
     playBtn.style.display = "none";
 
     let selectBox = document.querySelector("#selectBox");
     selectBox.style.display = "flex";
-
 }
 
+function openMapDialog() {
 
-function openMap() {
-
-
-    if (markerOnMiniMap != undefined) {
+     if (markerOnMiniMap !== undefined) {
         markerOnMiniMap.remove();
     }
-    if (userMarker != undefined) {
+    if (userMarker !== undefined) {
         userMarker.remove();
     }
 
-
-    const mapBox = document.querySelector("#newMap");
+    const mapBox = document.querySelector("#map");
+    const overlay = document.querySelector("#overlay");
 
     mapBox.style.display = "block";
-    mapBox.style.height = "80%";
+    mapBox.style.height = "60%";
     mapBox.style.width = "60%";
 
-    let selectBox2 = document.querySelector("#selectBox2");
-    selectBox2.style.display = "flex";
 
-    // Creates a mini popup map for the chosen lat and lng
-    if (!miniMap) {
-        // Skapa kartan om den inte redan finns
-        miniMap = L.map('map').setView([57.309, 14.637], 9); // Använd defaultvärden eller justera efter behov
+    overlay.style.display = "block";
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    if (miniMap === undefined) {
+        const mapBounds = L.latLngBounds(
+            [boundries.minLatCorner, boundries.minLngCorner],
+            [boundries.maxLatCorner, boundries.maxLngCorner]
+        );
+
+        miniMap = L.map('map', {
+            center: [56.8770, 14.8090], // Växjös koordinater
+            zoom: 13,
             minZoom: 8,
-            maxZoom: 18,
+            maxZoom: 20,
+            maxBounds: mapBounds,
             maxBoundsViscosity: 1,
-        }).addTo(miniMap);
+        });
 
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(miniMap);
         miniMap.on('click', function (event) {
             const markerPosition = event.latlng;
             markerOnMiniMap.setLatLng(markerPosition);
             userMarker.setLatLng(markerPosition);
         });
-
         miniMap.on("click", newUserMarker);
     }
 
-
     const ownPositionMarker = L.icon({
         iconUrl: "/mapIconsSVG/mapOwnPosition.svg",
-        iconSize: [20, 40],
-        iconAnchor: [10, 40]
+        iconSize: [24, 44],
+        iconAnchor: [12, 44]
     });
 
-    if (updateMapLoc(false) === "smaland") {
-        markerOnMiniMap = L.marker([57.309, 14.637], { icon: ownPositionMarker }).addTo(miniMap);
-        userMarker = L.marker([57.309, 14.637], { icon: ownPositionMarker }).addTo(map); 
-    } else {
-        markerOnMiniMap = L.marker([57.0, 16.0], { icon: ownPositionMarker }).addTo(miniMap); 
-        userMarker = L.marker([57.0, 16.0], { icon: ownPositionMarker }).addTo(map); 
-    }
+   // Sätter Växjö som default
+    markerOnMiniMap = L.marker([56.8770, 14.8090], { icon: ownPositionMarker }).addTo(miniMap);
+    userMarker = new L.marker([56.8770, 14.8090], { icon: ownPositionMarker }).addTo(miniMap);
 
+    let closeButton = document.querySelector("#closeButton");
+    closeButton.addEventListener("click", function() {
+        overlay.style.display = "none";
+    });
 }
 
+// Sätter en ny markör på kartan
 function newUserMarker(e) {
-    userMarker.setLatLng(e.latlng);
-    userMarker.addTo(map);
-
-    latitude = e.latlng.lat;
-    longitude = e.latlng.lng;
-}
-
-// Function that updates the position of the map with the geo-data
-function updateMapLoc(success) {
-    if (success) {
-        userMarker.remove();
-        userMarker = new L.marker([latitude, longitude], { icon: ownPositionMarker }).addTo(map);
-        map.setView([latitude, longitude], zoom = 16);
-        if (miniMap) {
-            miniMap.setView([latitude, longitude], zoom = 16);
-        }
+    if (userMarker) {
+        userMarker.setLatLng(e.latlng);
+        userMarker.addTo(miniMap);
+    } else {
+        userMarker = L.marker(e.latlng, {
+            icon: L.icon({
+                iconUrl: "/mapIconsSVG/mapOwnPosition.svg",
+                iconSize: [24, 44],
+                iconAnchor: [12, 44]
+            })
+        }).addTo(miniMap);
     }
     
+    const latitude = e.latlng.lat;
+    const longitude = e.latlng.lng;
+    console.log(`New marker set at latitude: ${latitude}, longitude: ${longitude}`);
 }
 
+function startGame() {
 
-// Closes the small popup map
-function closeMapDialog() {
-    latitude = userMarker._latlng.lat
-    longitude = userMarker._latlng.lng;
-    map.setView([latitude, longitude], zoom = 16);
-    const mapBox = document.querySelector("#map");
-    const overlay = document.querySelector("#overlay");
-
-    mapBox.style.display = "none";
     overlay.style.display = "none";
+    playBtn.style.display = "none";
+    selectBox.style.display = "none";
+
+
+    let gameBackground = document.querySelector("#boxBackground");
+    gameBackground.style.display = "flex";
+    console.log("SDFGH")
+
 }
+
+   
